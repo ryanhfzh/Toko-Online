fd4d6c7ebd01f09ee6e4919f5284c8ed8479b632
ryanhfzh <ryanhfzh@gmail.com>
2020-12-04 10:44:44 +0700

    up

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // return view('home');
+        $data = array(
+            'produks' => DB::table('products')->limit(10)->get(),
+        );
+        return view('user.welcome',$data);
+    }
+}
